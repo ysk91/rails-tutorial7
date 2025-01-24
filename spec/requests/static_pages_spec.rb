06006328ace0d39ba3703fb55fb10e6c2e1e47af
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-01-24 02:44:33 +0000

    aboutページを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,12 +10,24 @@ RSpec.describe 'static_pagesコントローラのテスト', type: :request do
     end
   end
 
-  describe 'helpページにアクセスできること' do
+  describe 'helpページ' do
     before do
       get static_pages_help_path
     end
     it 'レスポンスが200番を返すこと' do
       expect(response.status).to eq 200
+    end
+  end
+
+  describe 'aboutページ' do
+    before do
+      get static_pages_about_path
+    end
+    it 'レスポンスが200番を返すこと' do
+      expect(response.status).to eq 200
+    end
+    it 'タイトルが表示されること' do
+      expect(response.body).to include('About')
     end
   end
 end
