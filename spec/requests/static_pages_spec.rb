c12ae6d53ade825157c75331d5c5f79b7bf842f8
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-01-24 11:53:24 +0000

    パスの変更

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'static_pagesコントローラのテスト', type: :request do
 
   describe 'helpページ' do
     before do
-      get static_pages_help_path
+      get help_path
     end
     it 'レスポンスが200番を返すこと' do
       expect(response.status).to eq 200
@@ -27,13 +27,22 @@ RSpec.describe 'static_pagesコントローラのテスト', type: :request do
 
   describe 'aboutページ' do
     before do
-      get static_pages_about_path
+      get about_path
     end
     it 'レスポンスが200番を返すこと' do
       expect(response.status).to eq 200
     end
     it 'タイトルが正しく表示されること' do
       expect(response.body).to include('<title>About | Ruby on Rails Tutorial Sample App</title>')
+    end
+  end
+
+  describe 'contactページ' do
+    before do
+      get contact_path
+    end
+    it 'レスポンスが200番を返すこと' do
+      expect(response.status).to eq 200
     end
   end
 end
