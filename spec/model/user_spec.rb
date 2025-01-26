771eee8520c7eba75a18d2412c5c4d6e9cc3193f
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-01-26 07:26:54 +0000

    Userモデルの長さへのバリデーション追加

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -9,10 +9,22 @@ RSpec.describe 'Userモデルのテスト', type: :model do
         user.name = ''
         expect(user.valid?).to eq false
       end
+      it '50文字以下であること' do
+        user.name = 'a' * 50
+        expect(user.valid?).to eq true
+        user.name = 'a' * 51
+        expect(user.valid?).to eq false
+      end
     end
     context 'emailカラム' do
       it '空欄でないこと' do
         user.email = ''
+        expect(user.valid?).to eq false
+      end
+      it '255文字以下であること' do
+        user.email = 'a' * 243 + '@example.com'
+        expect(user.valid?).to eq true
+        user.email = 'a' * 244 + '@example.com'
         expect(user.valid?).to eq false
       end
     end
