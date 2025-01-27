24155f0b30bad0842df581f0bbf90c42d206a9f3
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-01-27 07:34:11 +0000

    usersテーブルにpasswordカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
+
+  has_secure_password
 end
