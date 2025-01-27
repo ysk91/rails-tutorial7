e299ee6cf335aa16d84f66009e47a26df703489f
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-01-27 17:41:25 +0900

    Userモデルにパスワードを追加する (#11)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "Example User" }
     email { "user@example.com" }
+    password { "P@ssw0rd" }
+    password_confirmation { "P@ssw0rd" }
   end
 end
