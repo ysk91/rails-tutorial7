15a8a048d5d8644410d111d0a394a255c8289e2f
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-02-04 12:54:06 +0000

    Spec修正

--- spec/system/users_login_spec.rb
+++ spec/system/users_login_spec.rb
@@ -19,7 +19,14 @@ RSpec.xdescribe 'サインアップ', type: :system do
     fill_in 'Password', with: ''
     fill_in 'Confirmation', with: ''
     click_button 'Create my account'
+
+    expect(page).to have_current_path(signup_path)
+
     expect(page).to have_content('The form contains 4 errors.')
+    expect(page).to have_content('Name can\'t be blank')
+    expect(page).to have_content('Email can\'t be blank')
+    expect(page).to have_content('Email is invalid')
+    expect(page).to have_content('Password is too short (minimum is 6 characters)')
 
     expect(User.count).to eq user_count
   end
