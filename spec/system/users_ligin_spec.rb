48859d4ddb0748f066ec3957594bddc8e6079c29
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-02-04 12:32:02 +0000

    サインアップのシステムスペックを作成

--- spec/system/users_ligin_spec.rb
+++ spec/system/users_ligin_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.xdescribe 'サインアップ', type: :system do
+RSpec.describe 'サインアップ', type: :system do
   it 'サインアップページにアクセスできること' do
     get root_url
     expect(page).to have_current_path(root_path)
@@ -8,5 +8,19 @@ RSpec.xdescribe 'サインアップ', type: :system do
     click_link 'Sign up now!'
     expect(page).to have_current_path(signup_path)
     exxpect(page).to have_content('<h1>Sign up</h1>')
+  end
+
+  it '無効な情報でサインアップするとエラーメッセージが表示されること' do
+    user_count = User.count
+
+    visit signup_path
+    fill_in 'Name', with: ''
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    fill_in 'Confirmation', with: ''
+    click_button 'Create my account'
+    expect(page).to have_content('The form contains 4 errors.')
+
+    expect(User.count).to eq user_count
   end
 end
