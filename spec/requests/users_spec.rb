58522ceed15435fcc739dbc1c23d622c06123b9d
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-02-06 10:40:34 +0900

    ユーザー登録機能を追加 (#12)

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,5 +8,30 @@ RSpec.describe 'usersコントローラのテスト', type: :request do
     it 'レスポンスが200番を返すこと' do
       expect(response.status).to eq 200
     end
+    it 'フォームが存在すること' do
+      forms = [
+        ['text', 'user[name]'],
+        ['text', 'user[email]'],
+        ['password', 'user[password]'],
+        ['password', 'user[password_confirmation]']
+      ]
+      forms.each do |type, name|
+        expect(response.body).to include "input class=\"form-control\" type=\"#{type}\" name=\"#{name}\""
+      end
+      expect(response.body).to include 'input type="submit"'
+    end
+  end
+
+  describe 'showページ' do
+    let!(:user) { create(:user) }
+    before do
+      get user_path(user)
+    end
+    it 'レスポンスが200番を返すこと' do
+      expect(response.status).to eq 200
+    end
+    it 'ユーザー名が表示されていること' do
+      expect(response.body).to include user.name
+    end
   end
 end
