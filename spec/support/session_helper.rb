48b6d2dc828dfd32dbc58694323c69dc25726d73
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-02-07 06:30:55 +0000

    logoutを実装

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,4 +1,4 @@
-module SpecSessionsHelper
+module SessionHelper
   def current_user_id
     page.get_rack_session_key('user_id')
   end
@@ -6,8 +6,16 @@ module SpecSessionsHelper
   def is_logged_in?
     current_user_id.present?
   end
+
+  def log_in_as(user)
+    visit login_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
 end
 
 RSpec.configure do |config|
-  config.include SpecSessionsHelper, type: :system
+  config.include SessionHelper, type: :system
 end
