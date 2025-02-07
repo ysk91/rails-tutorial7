89311abe2f123481d910d1b66873f7b559a766dc
Yusuke Sonoki <106280032+ysk91@users.noreply.github.com>
2025-02-07 08:25:01 +0000

    remember_meを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,12 +11,39 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  def self.digest(string)
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create(string, cost:)
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember
+    # userインスタンスに一時的にトークンを作成する（インスタンス変数に保存される）
+    self.remember_token = User.new_token
+    # userインスタンスのremember_digest属性にハッシュ化したトークンを保存する（データベースに保存される）
+    remember_digest = User.digest(remember_token)
+    update!(remember_digest:)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update!(remember_digest: nil)
+  end
+
+  class << self
+    private
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest(string)
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost:)
+    end
   end
 end
